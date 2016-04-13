9a49ae416e7c81842685d39f40d5da89a8a5c15b
JacksonWong <jackson.cw.wong@gmail.com>
2016-04-13 09:07:53 -0400

    some more fixes

--- custom_js_backup.js
+++ custom_js_backup.js
@@ -28,6 +28,41 @@ jQuery(document).ready(function( $ ) {
     var quotesNum = quotes.length;
     var randomCount = Math.floor(Math.random() * quotesNum);
     $(".orange_wisdom span").html(quotes[randomCount].textContent);
+    
+    //code for all animations on home page
+    if($(window).width() > 980){      
+        var $animation_elements = $('.animation_element');
+        var $window = $(window);
+        console.log($animation_elements);
+
+        function check_if_in_view() {
+          var window_height = $window.height();
+          var window_top_position = $window.scrollTop();
+          var window_bottom_position = (window_top_position + window_height);
+          console.log("window_top: " + window_top_position);
+          console.log("window_bottom: " + window_bottom_position);
+         
+          $.each($animation_elements, function() {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+         
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+              $element.addClass('in_view');
+              console.log(element_top_position);
+            } else {
+              $element.removeClass('in_view');
+            }
+          });
+        }
+
+        $window.on('resize', check_if_in_view);
+        $window.on('scroll', check_if_in_view);
+        $window.trigger('scroll');
+    }
 });
 
 // full_height_screen
