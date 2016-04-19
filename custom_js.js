08f528c50684f6c5f06060b2bb5dca7c8582a2da
JacksonWong <jackson.cw.wong@gmail.com>
2016-04-18 22:09:36 -0400

    making some changes to make take away the team portion

--- custom_js.js
+++ custom_js.js
@@ -1,19 +1,43 @@
 // all these functions need to be wrapped inside <script> tags
 
-//lines animation
+//facilities animation
+jQuery(document).ready(function( $ ) {
+    if($(window).width() > 980){
+        $(window).on('scroll', function() {
+            var y_scroll_pos = window.pageYOffset;
+            var one_stop_animation = 1400;
+            var bgSpeed = 2000;
+        
+            if(y_scroll_pos > one_stop_animation) {
+                $(".one_stop_shop_bg").animate({ right: '30px', opacity: '1' }, bgSpeed, 'linear');
+            }
+        });
+    }
+});
+
+//who_we_are animation
 jQuery(document).ready(function( $ ) {
     if($(window).width() > 980){
         $(window).on('scroll', function() {
             var y_scroll_pos = window.pageYOffset;
             var lines_animation = 700;
-            var animateSpeed = 1500;
+            var branded_animation = 1100;
+            var mission_animation = 1800;
+            var lineSpeed = 1500;
+            var bgSpeed = 2000;
         
             if(y_scroll_pos > lines_animation) {
-                $("#orange_segment_1 .line_box").animate({ bottom: '83px' }, animateSpeed, 'linear');
-                $("#orange_segment_2 .line_box").animate({ top: '80px' }, animateSpeed, 'linear');
-                $("#orange_segment_3 .line_box").animate({ bottom: '83px' }, animateSpeed, 'linear');
-                $("#orange_segment_4 .line_box").animate({ top: '80px' }, animateSpeed, 'linear');
-                $(".orange_segment_container .line_box").animate({ height: 'auto' }, animateSpeed, 'linear');
+                $("#orange_segment_1 .line_box").animate({ bottom: '83px' }, lineSpeed, 'linear');
+                $("#orange_segment_2 .line_box").animate({ top: '80px' }, lineSpeed, 'linear');
+                $("#orange_segment_3 .line_box").animate({ bottom: '83px' }, lineSpeed, 'linear');
+                $("#orange_segment_4 .line_box").animate({ top: '80px' }, lineSpeed, 'linear');
+                $(".orange_segment_container .line_box").animate({ height: 'auto' }, lineSpeed, 'linear');
+            }
+            if(y_scroll_pos > branded_animation) {
+                $(".branded_entertainment_section .branded_entertainment_bg").animate({ left: '0%', opacity: '1' }, bgSpeed, 'linear');
+            }
+            if(y_scroll_pos > mission_animation) {
+                $(".orange_mission_bg").animate({ right: '30px', opacity: '1' }, bgSpeed, 'linear');
             }
         });
     }
